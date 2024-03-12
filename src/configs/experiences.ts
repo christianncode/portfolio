8d49b059eb954554598766ae0a9410e5b475abcd
ChrisAnder1996 <ingeniero.sistemas.christian@gmail.com>
2024-03-12 12:28:11 -0500

    update experiences

--- src/configs/experiences.ts
+++ src/configs/experiences.ts
@@ -3,12 +3,12 @@ export const EXPERIENCES = [
     date: "Febrero 2022 - Enero 2024",
     title: "Frontend developer - Readiness IT",
     description:
-      "Responsable del desarrollo frontend para la implementación y actualización de nuevos componentes y datalayers, así como para solucionar incidencias reportadas por el cliente. Se configuraron algunas herramientas en el proyecto para mejorar la legibilidad del código y manejo de errores, asi como también se aplicaron buenas prácticas de clean code. Se logró mejorar la calidad del código e identificar mas rapido el error de alguna incidencia.",
+      "Responsable del desarrollo frontend para la implementación y actualización de nuevos componentes y datalayers, así como para solucionar incidencias reportadas por el cliente. Se configuraron algunas herramientas en el proyecto para mejorar la legibilidad del código y el manejo de errores, así como también se aplicaron buenas prácticas de clean code. Se logró mejorar la calidad del código e identificar más rápido el error de alguna incidencia.",
   },
   {
     date: "Febrero 2020 - Diciembre 2021",
     title: "Frontend developer - Avatar S.A.C",
     description:
-      "Encargado de asumir parte del liderazgo del equipo frontend y responsable de implementar datalayers, solucionar incidencias, construir y actualizar las interfaces de usuario de diferentes e-commerce como Ripley, Coppel México y todas las marcas pertenecientes a la compañía colombiana Crystal S.A.S., las cuales son: Gef, Punto Blanco, Galax y Babyfresh.",
+      "Encargado de asumir parte del liderazgo del equipo frontend y responsable de implementar datalayers, solucionar incidencias, construir y actualizar las interfaces de usuario de diferentes e-commerce como Ripley, Coppel México y todas las marcas pertenecientes a la compañía colombiana Crystal S.A.S., las cuales son: Gef, Punto Blanco, Galax y Babyfresh. Logré aprender y asumir nuevos retos con la implementación de datalayers con javascript.",
   },
 ];
